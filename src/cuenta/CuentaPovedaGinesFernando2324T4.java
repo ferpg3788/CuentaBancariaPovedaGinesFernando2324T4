fe0a984b09f223277214dd7498c2805558760799
ferpg <ferpg@Fer>
2024-02-25 20:25:58 +0100

    Mi proyecto PovedaGinesFernando2324 comentado con javadoc

--- src/cuenta/CuentaPovedaGinesFernando2324T4.java
+++ src/cuenta/CuentaPovedaGinesFernando2324T4.java
@@ -1,6 +1,11 @@
 package cuenta;
 
 
+/** 
+ * Clase CuentaPovedaGinesFernando2324T4, para realizar las operaciones asignadas a la cuenta
+ */
+
+
 public class CuentaPovedaGinesFernando2324T4 {
 
 
@@ -10,11 +15,21 @@ public class CuentaPovedaGinesFernando2324T4 {
     private double tipoInteres;
 
    
+    /**Constructor sin argumentos*/
+    
     public CuentaPovedaGinesFernando2324T4 ()
     {
     }
    
    
+    /**
+     * Constructor con argumentos
+     * @param nom nombre del titular de la cuenta
+     * @param cue numero de cuenta
+     * @param sal saldo de la cuenta
+     * @param tipo tipo de interes de la cuenta
+     */
+    
     public CuentaPovedaGinesFernando2324T4 (String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -24,11 +39,21 @@ public class CuentaPovedaGinesFernando2324T4 {
     }
    
     
+    /**
+     * Método asignarNombre para asignar un nombre a la cuenta
+     * @param nom nombre del titular de la cuenta
+     */
+    
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
    
+    
+    /**
+     * Método obtenerNombre que devuelve el nombre asociado a la cuenta
+     * @return devuelve nombre asociado a la cuenta
+     */
    
     public String obtenerNombre()
     {
@@ -36,11 +61,23 @@ public class CuentaPovedaGinesFernando2324T4 {
     }
 
     
+    /**
+     * Método estado que devuelve el saldo de la cuenta
+     * @return devuelve saldo de la cuenta
+     */
+    
      public double estado ()
     {
         return getSaldo();
     }
 
+     
+     /**
+      * Método para ingresar dinero en la cuenta, si la cantidad es correcta suma la cantidad ingresada al saldo inicial
+      *@param cantidad cantidad a ingresar
+      *@param concepto concepto de ingreso
+      *@throws Exception si la cantidad es menor que 0.
+      */
    
     public void ingresar(double cantidad, String concepto) throws Exception
     {
@@ -49,6 +86,13 @@ public class CuentaPovedaGinesFernando2324T4 {
         setSaldo(getSaldo() + cantidad);
     }
 
+    
+    /**
+     * Método para retirar dinero de la cuenta, si la cantidad es correcta resta la cantidad retirada al saldo inicial
+     * @param cantidad  cantidad a retirar 
+     * @throws Exception si la cantidad es igual o menor que cero
+     * @throws Exception si la cantidad igual o mayor que el saldo de la cuenta
+     */    
     
     public void retirar (double cantidad) throws Exception
     {
@@ -59,6 +103,10 @@ public class CuentaPovedaGinesFernando2324T4 {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * Método para obtener el número de cuenta asociado
+     * @return obtener número de cuenta
+     */
     
     public String obtenerCuenta ()
     {
@@ -66,56 +114,66 @@ public class CuentaPovedaGinesFernando2324T4 {
     }
 
     /**
-     * @return the nombre
+     * Método para obetner el nombre del titular de la cuenta
+     * @return obtener el nombre del titular
      */
+    
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Método para asignar/cambiar el nombre del titular de la cuenta
+     * @param nombre nombre del titular de la cuenta
      */
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Método para obtener el número de cuenta
+     * @return número de cuenta
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Método para asignar/cambiar el número de cuenta
+     * @param cuenta número de cuenta
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Método que devuelve el saldo actual de la cuenta
+     * @return saldo de cuenta
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Método para asignar/cambiar el valor del saldo de la cuenta
+     * @param saldo saldo de la cuenta
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Método que devuelve el tipo de interés asociado la cuenta
+     * @return tipo de interés
      */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Método para asignar/cambiar el valor del tipo de interés
+     * @param tipoInteres tipo de interés
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
--- src/cuenta/Main.java
+++ src/cuenta/Main.java
@@ -1,7 +1,19 @@
 package cuenta;
 
+/** 
+ * Clase main del programa, que define un objeto cuenta con unos parámetros y realiza acciones de ingresar y retirar dinero de la cuenta
+ * 
+ * @author ferpg
+ */
+
 
 public class Main {
+    
+    /**
+     * Método Main
+     * 
+     * Este método es el princinpal de ejecución del programa
+     */
 
     
     public static void main(String[] args) {
@@ -11,6 +23,7 @@ public class Main {
         
         miCuentaPovedaGinesFernando2324T4 = new CuentaPovedaGinesFernando2324T4("Antonio Sánchez","ES55-3058-2365-8522-1234-5678",2500,0);
         System.out.println("Saldo inicial de la cuenta: "+miCuentaPovedaGinesFernando2324T4.estado()+"€");
+        
         retiraDineroPovedaGinesFernando(miCuentaPovedaGinesFernando2324T4);
         
         ingresaDineroPovedaGinesFernando(miCuentaPovedaGinesFernando2324T4);
@@ -18,8 +31,24 @@ public class Main {
         System.out.println("El saldo actual es "+ saldoActual );
     }
 
+    
+    
+    /** Método ingresaDineroPovedaGinesFernando.
+     * 
+     * En este método se va a realizar la acción de ingresar 100 € a la cuenta
+     * 
+     * Al ingresar los 100 € no saltará la excepción y se aumentará el saldo de la cuenta en 100 €
+     * 
+     * @param miCuentaPovedaGinesFernando2324T4 objeto cuenta a ingresar dinero
+     * @throws Excepción si la cantidad a ingresar es negativao 0.
+     */
+    
+    
     private static void ingresaDineroPovedaGinesFernando(CuentaPovedaGinesFernando2324T4 miCuentaPovedaGinesFernando2324T4) {
         int x;
+        
+        /*Vamos a intentar ingresar 100 € para lo cual no debe saltar la excepción y devolvernos el importe del saldo inicial más la cantidad ingresada*/
+        
         try
         {
             x=100;
@@ -31,8 +60,27 @@ public class Main {
         }
     }
 
+    
+    
+    /**Método retiraDInroPovedaGinesFernando
+     * 
+     * En este método se va a realizar la acción de retirar 1.500€ de la cuenta
+     * 
+     * Al retirar los 1.500 € no saltará la excepción y se disminuirá el saldo de la cuenta en 1.500 €
+     * 
+     * @param miCuentaPovedaGinesFernando2324T4 objeto cuenta de la que retirar dinero
+     * @throws Excepción si la cantidad a retirar es igual o menor que 0, o si el saldo de la cuenta es menor o igual que la cantidad a retirar
+     */
+    
+    
+    
     private static void retiraDineroPovedaGinesFernando(CuentaPovedaGinesFernando2324T4 miCuentaPovedaGinesFernando2324T4) {
         int x;
+       
+        /*Vamos a intentar retirar 1.500 € para lo cual no debe saltar la excepción ya que tenemos un saldo incial de 2.500, nos debe devolver el importe del saldo inicial menos
+         * la cantidad retirada
+         */
+        
         try
         {
             x=1500;
